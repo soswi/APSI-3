b44815ee0429cda081aaad7d98716bcb095a7875
Wiktor Sosnowski <wiktorsoswisosnowski@gmail.com>
2026-06-16 14:14:26 +0200

    fix: fixed issue with random path

--- backend/startup.sh
+++ backend/startup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+VENV_PATH=$(find /tmp -name "activate" -path "*/antenv/*" 2>/dev/null | head -1 | sed 's|/bin/activate||')
+source $VENV_PATH/bin/activate
 cd /home/site/wwwroot/backend
-../antenv/bin/pip install -r requirements.txt
-../antenv/bin/gunicorn apsi_3.wsgi:application --bind=0.0.0.0:8000
+gunicorn apsi_3.wsgi:application --bind=0.0.0.0:8000
