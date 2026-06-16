a61ab548a5d2cbdca22807fb6532d63f906e1ca2
Wiktor Sosnowski <wiktorsoswisosnowski@gmail.com>
2026-06-16 15:10:16 +0200

    feat: again with startup

--- backend/startup.sh
+++ backend/startup.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-VENV_PATH=$(find /tmp -name "activate" -path "*/antenv/*" 2>/dev/null | head -1 | sed 's|/bin/activate||')
-source $VENV_PATH/bin/activate
-cd /home/site/wwwroot/backend
-gunicorn apsi_3.wsgi:application --bind=0.0.0.0:8000
+cd /home/site/wwwroot
+
+# Rozpakuj antenv jeśli nie jest jeszcze rozpakowany
+if [ ! -f antenv/lib/python3.11/site-packages/django/__init__.py ]; then
+    echo "Rozpakowywanie antenv.tar.gz..."
+    tar -xzf antenv.tar.gz
+fi
+
+antenv/bin/gunicorn apsi_3.wsgi:application --bind=0.0.0.0:8000
